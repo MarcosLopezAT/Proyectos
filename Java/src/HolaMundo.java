0c69648a4bd0ebeafd9f8811a58e8c322354210f
Marcos López <marcoslopezat@gmail.com>
2022-01-12 23:06:44 +0100

    Se agregan variables númericas al archivo de Java PruebasX

--- Java/src/HolaMundo.java
+++ Java/src/HolaMundo.java
@@ -2,7 +2,14 @@ public class HolaMundo {
 
     public static void main(String[] args) {
         System.out.println("Hola IntelliJ idea");
-;
+        //Variables numericas
+
+        byte nB=123; // 1 bit
+        int n=1234567890; //4 bits
+        long nL=12345678910L; // 8 bits
+
+        float dF=11.2F; // 4 bits
+        double d=1.255; // bits
 
     }
 }
